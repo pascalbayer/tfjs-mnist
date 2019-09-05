89e25d11397b63bc82998dbf55ea99360f8b2ca7
Pascal Bayer <pascal.bayer@cubesoft.org>
2019-09-05 15:07:02 +0200

    New blank setup

--- model.ts
+++ model.ts
@@ -6,38 +6,4 @@ import { IMAGE_H, IMAGE_W } from './data';
  *
  * @returns {tf.Model} An instance of tf.Model.
  */
-export function createConvModel(): tf.Sequential {
-    const model = tf.sequential();
-
-    model.add(
-        tf.layers.conv2d({
-            inputShape: [IMAGE_H, IMAGE_W, 1],
-            kernelSize: 5,
-            filters: 32,
-            activation: 'relu'
-        })
-    );
-
-    model.add(tf.layers.maxPooling2d({ poolSize: 2, strides: 2 }));
-
-    model.add(
-        tf.layers.conv2d({
-            inputShape: [IMAGE_H, IMAGE_W, 1],
-            kernelSize: 5,
-            filters: 64,
-            activation: 'relu'
-        })
-    );
-
-    model.add(tf.layers.maxPooling2d({ poolSize: 2, strides: 2 }));
-
-    model.add(tf.layers.flatten({}));
-
-    model.add(tf.layers.dense({ units: 128, activation: 'relu' }));
-
-    model.add(tf.layers.dropout({ rate: 0.1 }));
-
-    model.add(tf.layers.dense({ units: 10, activation: 'softmax' }));
-
-    return model;
-}
+export function createConvModel(): tf.Sequential {}
